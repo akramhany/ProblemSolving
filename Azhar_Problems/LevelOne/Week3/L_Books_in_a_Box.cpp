0292d2cf3cfcacdfad90be895ea07f925fcbfc56
akramhany <akramhany65@gmail.com>
2024-07-27 16:48:47 +0300

    upload problems

---
+++ Azhar_Problems/LevelOne/Week3/L_Books_in_a_Box.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <string>
+#include <set>
+#include <cstring>
+#include <stack>
+#include <queue>
+
+#define ll long long
+#define llu unsigned long long
+
+using namespace std;
+
+bool isPrime(ll n)
+{
+  if (n == 2)
+    return true;
+  else if (n < 2 || n % 2 == 0)
+    return false;
+
+  for (ll i = 3; i * i <= n; i += 2)
+  {
+    if (n % i == 0)
+      return false;
+  }
+
+  return true;
+}
+
+ll sumDivisors(ll n)
+{
+  ll sum = 0;
+
+  for (ll i = 1; i * i <= n; i++)
+  {
+    if (i * i == n)
+    {
+      sum += i;
+    }
+    else if (n % i == 0)
+    {
+      sum += i;
+      sum += (n / i);
+    }
+  }
+
+  return sum;
+}
+
+// a must be the bigger number
+ll gcd(ll a, ll b)
+{
+  if (b == 0)
+    return a;
+
+  return gcd(b, a % b);
+}
+
+// a must be the bigger number
+ll lcm(ll a, ll b)
+{
+  return (ll)(a * b) / gcd(a, b);
+}
+
+int main()
+{
+  cin.tie(0);
+  cin.sync_with_stdio(0);
+
+  int n;
+  cin >> n;
+
+  stack<int> st;
+  vector<int> res;
+
+  while (n--)
+  {
+    int k;
+    cin >> k;
+
+    if (k == 1)
+    {
+      int id;
+      cin >> id;
+
+      st.push(id);
+    }
+    else if (k == 2 && !st.empty())
+      st.pop();
+    else if (k == 3 && !st.empty())
+      res.push_back(st.top());
+  }
+
+  for (auto &i : res)
+    cout << i << "\n";
+}
